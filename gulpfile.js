f62b412b2383db2e77e6c013e18d254e2502d068
Zhihao <cabtravis@gmail.com>
2015-01-23 04:27:22 +0800

    [npm] change version for some dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 
 require('./gulptask')([
-  'browserify',
-  'sass',
-  'watch',
-  'copyFiles',
-  'server'
+    'browserify',
+    'sass',
+    'watch',
+    'copyFiles',
+    'server'
 ]);
 
 gulp.task('default', ['watch', 'browserify', 'sass']);
-gulp.task('build',['browserify', 'sass','copyFiles']);
+gulp.task('build', ['browserify', 'sass', 'copyFiles', 'server']);
